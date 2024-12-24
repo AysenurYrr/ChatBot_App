9773c2d0bef29242978aa13dd1fee5d44e989025
AysenurYrr <aysenuryorur0@gmail.com>
2024-12-24 23:26:38 +0300

    Folders added

---
+++ ai_chatbot/lib/helper/global.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Bir widget veya değişkenin değişmeyeceğini garanti ediyorsanız, const kullanabilirsiniz.
+const appName = 'Ai Assistant';
+
+// Ekran boyutu için tanımlanan değişken
+late Size mq;
+// Ekran boyutları gibi bazı değerler, widget yapısı tamamen yüklendikten sonra elde edilir. 
+// Bu nedenle bu tür değişkenler late olarak tanımlanır.
+// late, değişkenin kullanılmadan önce mutlaka bir değer almasını garanti eder. 
+// Eğer bir değer verilmeden kullanılmaya çalışılırsa bir hata oluşur.
+// Size mq ise kullanılan ekran boyutlarını ifade eden bir değişkendir 
+
+// Google Gemini API Key kullanmak için global değişken atayalım. Aşağıdaki adresten alabiliriz.
+// https://aistudio.google.com/app/apikey
+String apiKey = 'Burayi ';
+String projeNo = 'deis';
+
+// Uygulamada kullanılacak global değişkenleri burada tanımladık.
